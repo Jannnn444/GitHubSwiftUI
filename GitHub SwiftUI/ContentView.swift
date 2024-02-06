c67d892e59e094fab77a5372fd9eae39039c0217
Jannnn444 <138713229+Jannnn444@users.noreply.github.com>
2024-02-06 12:10:42 +0800

    Initial Commit

---
+++ GitHub SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHub SwiftUI
+//
+//  Created by yucian huang on 2024/2/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHub SwiftUI/GitHub_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_SwiftUIApp.swift
+//  GitHub SwiftUI
+//
+//  Created by yucian huang on 2024/2/6.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
